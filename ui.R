52836d9923c32f46b92b17221f1b52428ac2962c
Phil <33665495+nguyenhphilip@users.noreply.github.com>
2019-07-18 15:36:58 -0400

    Update UI

--- ui.R
+++ ui.R
@@ -2,13 +2,11 @@ ui <- fluidPage(
   titlePanel(title="ABCD Database Builder 2.0"),
   sidebarLayout(
     sidebarPanel(
-<<<<<<< HEAD
       tags$h3("How the app works:"),
       tags$p("This Web App that allows you to build and download a large spreadsheet containing all the variables 
              you're interested in analyzing from the ABCD Dataset."),
       tags$p("If you don't want to deal with selecting specific variables, simply leave the second textbox blank, 
              and the downloadable file will contain all the variables contained in the spreadsheets you've selected."),
-      br(),
       tags$h4("VARIABLES AUTOMATICALLY INCLUDED:"),
       tags$ul(
         tags$li("Subject ID"),
@@ -25,14 +23,8 @@ ui <- fluidPage(
       br(),
       tags$h4(tags$a(href = "https://nda.nih.gov/data_dictionary.html?source=ABCD%2BRelease%2B2.0&submission=ALL",
                      target = "_blank",
-             "**Check out the ABCD Data Dictionary to find your spreadsheets and variables of interest. Then: **")),
+             "Check out the ABCD Data Dictionary to find your spreadsheets and variables of interest. Then:")),
       br(),
-=======
-      tags$h4(tags$a(href = "https://nda.nih.gov/data_dictionary.html?source=ABCD%2BRelease%2B2.0&submission=ALL",
-                     target = "_blank",
-             "**Check out the ABCD Data Dictionary to find your spreadsheets and variables of interest.**")),
-      hr(),
->>>>>>> 0919832ef671684acceb99f13c69eb63d666113a
       selectInput(inputId = "datasets",
                   label = "1) Search for and select your dataset(s):",
                   selected = "",
@@ -41,26 +33,12 @@ ui <- fluidPage(
       ),
       hr(),
       selectInput(inputId = "variables",
-                  label = "2) Select variables of interest (NOTE: Leave blank if you want all variables from all spreadsheets 
+                  label = "2) Select variables of interest (NOTE: Leave blank if you want all variables from all selected spreadsheets 
                   to be included.)",
                   choices = "",
                   multiple = TRUE
       ),
-<<<<<<< HEAD
 
-=======
-      tags$h5("**VARIABLES AUTOMATICALLY INCLUDED:**"),
-      tags$ul(
-        tags$li("Subject ID"),
-        tags$li("Event Type"),
-        tags$li("Age"),
-        tags$li("Sex"),
-        tags$li("Combined Parent Income"),
-        tags$li("Parent highest education"),
-        tags$li("Partner highest education"),
-        tags$li("Site ID"),
-        tags$li("Family ID")),
->>>>>>> 0919832ef671684acceb99f13c69eb63d666113a
       hr(),
       textInput(inputId = "title",
                 label = "3) Name the download file:",
@@ -74,15 +52,12 @@ ui <- fluidPage(
                       target = "_blank",
                       "Phil Nguyen, ABCD Study Research Assistant @ UVM")
                )
-<<<<<<< HEAD
              ),
       tags$p("Source",
              tags$a(href = "https://github.com/nguyenhphilip/ABCD_Data_Explorer",
                     target = "_blank",
                     "code."
                     )
-=======
->>>>>>> 0919832ef671684acceb99f13c69eb63d666113a
              )
       ),
     mainPanel(
