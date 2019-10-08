f6265f4719761569c91425e2a4446e15c22bc035
Phil <33665495+nguyenhphilip@users.noreply.github.com>
2019-10-08 11:57:01 -0400

    Update reactive spreadsheet and removed match restriction

--- server.R
+++ server.R
@@ -1,7 +1,29 @@
 server <- function(input, output, session) {
+  
+  # create a list of spreadsheets to be merged and returned at the end
+  spreadsheet = reactive({
+    files <- list()
+    spreads <- list.files(here("2.0-ABCD-Release-updated"))
+    for(spread in spreads){
+      if(gsub(".csv","",spread) %in% input$datasets)
+        files[[length(files)+1]] <- here("2.0-ABCD-Release-updated", spread)
+    }
+    csv_files <- lapply(files, function(i){
+      readr::read_csv(i) %>% select(-dataset_id)
+    })
+    if(length(csv_files) > 1){
+      reduced_files <- csv_files %>% reduce(full_join)
+      final_spread <- covariates %>% full_join(reduced_files, by = c("src_subject_id","eventname"))
+    }
+    else if(length(csv_files) == 1){
+      final_spread <-  covariates %>% full_join(csv_files[[1]], by = c("src_subject_id", "eventname"))
+    }
+    return(final_spread)
+  })
+  
   outVar = reactive({
-    # reactively return a list of variables based on the category(s) and dataset(s) selected
-    return(createVars(input$datasets))
+    # reactively return a list of variables based on thedataset(s) selected
+    return(names(spreadsheet()))
   })
   
   observe({
@@ -14,37 +36,20 @@ server <- function(input, output, session) {
                         choices = "")
     }
   })
-  # create a list of spreadsheets to be merged and returned at the end
-  spreadsheet = reactive({
-    files <- list()
-      spreads <- list.files(here("2.0-ABCD-Release-R-format"))
-      for(spread in spreads){
-        if(gsub(".Rds","",spread) %in% input$datasets)
-          files[[length(files)+1]] <- here("2.0-ABCD-Release-R-format", spread)
-      }
-    csv_files <- lapply(files, function(i){
-      readRDS(file = i)
-    })
-    if(length(input$variables) > 0 & length(csv_files)){
-      reduced_files <- csv_files %>% reduce(full_join)
-      final_spread <- covariates %>% full_join(reduced_files, by = c("src_subject_id","eventname")) %>% 
-        select(covariate_vars,input$variables)
-    } else if(length(input$variables) == 0 & length(csv_files) > 1){
-      reduced_files <- csv_files %>% reduce(full_join)
-      final_spread <- covariates %>% full_join(reduced_files, by = c("src_subject_id","eventname"))
-    }
-    else if(length(input$variables) == 0 & length(csv_files) == 1){
-      final_spread <-  covariates %>% full_join(csv_files[[1]], by = c("src_subject_id", "eventname"))
-    }
-    return(final_spread)
-  })
-  
+
   output$table <- DT::renderDataTable({
     req(input$datasets)
-    DT::datatable(data = spreadsheet(),
+    if(length(input$variables) == 0){
+      DT::datatable(data = spreadsheet(),
+                    options = list(pageLength = 10,
+                                   scrollX = TRUE),
+                    rownames = FALSE)
+    } else {
+    DT::datatable(data = spreadsheet() %>% select(input$variables),
                   options = list(pageLength = 10,
                                  scrollX = TRUE),
                   rownames = FALSE)
+    }
   })
   
   output$download_data <- downloadHandler(
@@ -56,7 +61,7 @@ server <- function(input, output, session) {
       }
     },
     content = function(file){
-      write.csv(spreadsheet(), file, row.names = FALSE, na = "")
+      write_csv(spreadsheet() %>% select(input$variables), file, na = "")
     }
   )
 }
